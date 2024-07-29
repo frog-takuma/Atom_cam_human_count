840cf45d07a487b98b036ac417cfa5eee10c297c
ubuntu_frogtakuma <kashihara.yuto@gmail.com>
2024-07-29 19:52:00 +0900

    test

---
+++ molten_analyze.py
@@ -0,0 +1,11 @@
+import cv2
+import time
+
+import glob
+import os
+from openpyxl import Workbook
+from ultralytics import YOLO
+from PIL import Image
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
